d2f4065d3446c1fada33d0ad7d5a50ad20a6dba9
Likit Preeyanon <likit.pre@mahidol.edu>
2021-11-12 00:02:58 +0700

    removed unused menu items

--- src/components/navMenu.vue
+++ src/components/navMenu.vue
@@ -6,16 +6,6 @@
       <b-navbar-item :to="{ name: 'MainPage' }" tag="router-link">
         Home
       </b-navbar-item>
-      <b-navbar-dropdown label="Programs">
-        <b-navbar-item>
-          Main Page
-        </b-navbar-item>
-      </b-navbar-dropdown>
-      <b-navbar-dropdown label="Creator">
-        <b-navbar-item>
-          Main Page
-        </b-navbar-item>
-      </b-navbar-dropdown>
     </template>
   </b-navbar>
 </template>
